dec0116074b653b3d10abda7538578700184deaf
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-05-15 20:06:10 +0900

    interface 사용해보기

--- typescript/ts-practice/dist/practice.js
+++ typescript/ts-practice/dist/practice.js
@@ -1,5 +1,23 @@
 "use strict";
-function sum(x, y) {
-    return x + y;
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수를 구현합니다.
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
 }
-sum(1, 2);
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach((shape) => {
+    console.log(shape.getArea());
+});
--- typescript/ts-practice/src/practice.ts
+++ typescript/ts-practice/src/practice.ts
@@ -1,4 +1,37 @@
-function sumArray(numbers: number[]): number {
-  return numbers.reduce((acc, cur) => acc + cur, 0);
+// Shape 라는 interface 를 선언
+interface Shape {
+  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자
 }
-const total = sumArray([1, 2, 3, 4, 5]);
+
+class Circle implements Shape {
+  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시
+  constructor(public radius: number) {
+    this.radius = radius;
+  }
+
+  // 너비를 가져오는 함수를 구현
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius); // 에러 없이 작동
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
