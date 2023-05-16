d9bd89247908ad5a51192b22d768acfef2b6106d
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-05-16 16:10:11 +0900

    일반 객체를 interface로 타입 설정하기

--- typescript/ts-practice/dist/practice.js
+++ typescript/ts-practice/dist/practice.js
@@ -1,23 +1,10 @@
 "use strict";
-class Circle {
-    constructor(radius) {
-        this.radius = radius;
-    }
-    // 너비를 가져오는 함수를 구현합니다.
-    getArea() {
-        return this.radius * this.radius * Math.PI;
-    }
-}
-class Rectangle {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    getArea() {
-        return this.width * this.height;
-    }
-}
-const shapes = [new Circle(5), new Rectangle(10, 5)];
-shapes.forEach((shape) => {
-    console.log(shape.getArea());
-});
+const person = {
+    name: "김사람",
+    age: 20,
+};
+const expert = {
+    name: "갬개발",
+    skills: ["typescript", "nextjs"],
+};
+const people = [person, expert];
--- typescript/ts-practice/src/practice.ts
+++ typescript/ts-practice/src/practice.ts
@@ -1,37 +1,16 @@
-// Shape 라는 interface 를 선언
-interface Shape {
-  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자
+interface Person {
+  name: string;
+  age?: number;
 }
-
-class Circle implements Shape {
-  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시
-  constructor(public radius: number) {
-    this.radius = radius;
-  }
-
-  // 너비를 가져오는 함수를 구현
-  getArea() {
-    return this.radius * this.radius * Math.PI;
-  }
+interface Developer extends Person {
+  skills: string[];
 }
-
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  getArea() {
-    return this.width * this.height;
-  }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius); // 에러 없이 작동
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach((shape) => {
-  console.log(shape.getArea());
-});
+const person: Person = {
+  name: "김사람",
+  age: 20,
+};
+const expert: Developer = {
+  name: "갬개발",
+  skills: ["typescript", "nextjs"],
+};
+const people: Person[] = [person, expert];
