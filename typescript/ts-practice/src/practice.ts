0e6af4237d3f9b62929a9a2170bcc66f6e5ded90
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-05-16 16:32:54 +0900

    Generics: 함수에서 Generics 사용하기

--- typescript/ts-practice/src/practice.ts
+++ typescript/ts-practice/src/practice.ts
@@ -1,25 +1,6 @@
-type Person = {
-  name: string;
-  age?: number;
-};
-
-type Developer = Person & {
-  //&는 인터섹션으로 두 개 이상의 타입들을 합쳐준다.
-  skills: string[];
-};
-
-const person: Person = {
-  name: "김사람",
-};
-
-const expert: Developer = {
-  name: "김개발",
-  skills: ["typescript", "nextjs"],
-};
-
-type People = Person[]; //Person[]을 People 이라는 타입으로 사용할 수 있다.
-const people: People = [person, expert];
-
-type Color = "red" | "orange" | "yellow";
-const color: Color = "red";
-const colors: Color[] = ["red", "orange"];
+function wrap<T>(param: T) {
+  return {
+    param,
+  };
+}
+const wrapped = wrap(10);
