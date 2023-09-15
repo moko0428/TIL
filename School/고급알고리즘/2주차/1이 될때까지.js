2bf07cd2caf8e39c22366277ba27bacda0720bfb
Leejunyoung <ljun925@naver.com>
2023-09-15 20:35:59 +0900

    1주차 그리드 1이 될때까지 (자바스크립트)

---
+++ School/고급알고리즘/2주차/1이 될때까지.js
@@ -0,0 +1,15 @@
+function solution(n, k){
+  let count = 0;
+
+  while(n > 1){
+    if( n%k === 0){
+      // n이 k로 나누어 떨어지는 경우
+      n /= k;
+    } else {
+      // n이 k로 나누어 떨어지지 않는 경우
+      n--;
+    }
+    count++;
+  }
+  return count;
+}
